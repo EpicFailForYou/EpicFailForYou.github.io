e72884a64332524454f95872220f7b818a7c2038
EpicFailForYou <49729492+EpicFailForYou@users.noreply.github.com>
2023-01-28 15:56:57 -0500

    Initial Push

---
+++ script.js
@@ -0,0 +1,30 @@
+const socket = io('http://44.203.44.104:3000/', {
+    cors: {
+      origin: ["https://admin.socket.io"],
+      credentials: true
+    }
+  });
+//const socket = require('socket.io-client')('http://localhost:3000');
+socket.on('connect', () => {
+    console.log(`You connected with id: ${socket.id}`);
+    socket.emit('sendMessage', "THIS IS MY ANSWER")
+})
+
+
+//User typed in a room code and attemped to join it
+function joinRoom() {
+    const elem = document.getElementById('roomCode').innerText;
+    socket.emit("requestToJoin", elem, message => {
+        console.log(message);
+    })
+}
+
+//Let player know that they cannot join that room
+function roomJoinFailed() {
+
+}
+
+//User submitted an answer to be sent to the server
+function submitSimpleAnswer() {
+
+}
